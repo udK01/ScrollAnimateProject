42ac224e7f33d16be1b6a7986b650964fd95d9fc
udK01 <dominik.muranyi@gmail.com>
2024-07-04 19:52:17 +0100

    Added simple on scroll animation.

--- ScrollAnimateProject/src/App.jsx
+++ ScrollAnimateProject/src/App.jsx
@@ -1,11 +1,30 @@
-import AboutMe from "./components/AboutMe";
-import Sidebar from "./components/Sidebar";
+import React, { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    return () => {
+      hiddenElements.forEach((el) => observer.unobserve(el));
+    };
+  }, []);
+
   return (
-    <section className="flex">
-      <Sidebar />
-      <AboutMe />
+    <section className="flex flex-col items-center bg-sky-700 text-orange-500 font-bold space-y-10">
+      <div className="box hidden">Welcome!</div>
+      <div className="box hidden">This is a test!</div>
+      <div className="box hidden">What are we testing?</div>
     </section>
   );
 }
--- ScrollAnimateProject/src/components/AboutMe.jsx
+++ ScrollAnimateProject/src/components/AboutMe.jsx
@@ -1,9 +1,14 @@
 export default function AboutMe() {
   return (
     <section className="bg-midTone h-screen w-full">
-      <div className="font-poppins text-center">
-        <span className="text-white text-[100px]">Company Name</span>
-        <span className="text-orange-500 text-[150px]">.</span>
+      <div className="flex flex-col items-center">
+        <div className="font-poppins text-center">
+          <span className="text-white text-[100px]">Company Name</span>
+          <span className="text-orange-500 text-[150px]">.</span>
+        </div>
+        <span className="text-white text-[30px] -translate-y-[30px]">
+          "slogan slogan slogan."
+        </span>
       </div>
     </section>
   );
