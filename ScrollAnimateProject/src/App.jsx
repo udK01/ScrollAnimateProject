41c76bf7d1e37c5d0458bd87238809f5c78fd36f
udK01 <dominik.muranyi@gmail.com>
2024-07-06 18:50:38 +0100

    Added fade out and fade in with different text animation effect.

--- ScrollAnimateProject/src/App.jsx
+++ ScrollAnimateProject/src/App.jsx
@@ -1,30 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  const elements = ["Developer", "Programmer", "Designer"];
+
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        } else {
-          entry.target.classList.remove("show");
-        }
-      });
-    });
+    const intervalId = setInterval(() => {
+      setCounter((prevCounter) => (prevCounter + 1) % elements.length);
+    }, 2000);
 
-    const hiddenElements = document.querySelectorAll(".hidden");
-    hiddenElements.forEach((el) => observer.observe(el));
-
-    return () => {
-      hiddenElements.forEach((el) => observer.unobserve(el));
-    };
-  }, []);
+    return () => clearInterval(intervalId);
+  }, [elements.length]);
 
   return (
-    <section className="flex flex-col items-center bg-sky-700 text-orange-500 font-bold space-y-10">
-      <div className="box hidden">Welcome!</div>
-      <div className="box hidden">This is a test!</div>
-      <div className="box hidden">What are we testing?</div>
+    <section className="flex justify-center text-[100px] bg-sky-950 text-white h-screen">
+      <div>
+        <div className="flex">
+          <div>Hi, I am</div>&nbsp;
+          <div>udK</div>
+          <div className="text-orange-500">.</div>
+        </div>
+        <div className="flex text-[70px]">
+          <div>I am a&nbsp;</div>
+          <div>
+            <div>
+              {elements.map((element, index) => (
+                <div
+                  key={index}
+                  className={`absolute transition-all ${
+                    counter === index
+                      ? "opacity-100 blur-0 underline translate-y-0"
+                      : "opacity-0 blur-md no-underline text- translate-y-2"
+                  }`}
+                  style={{
+                    transitionDuration: "750ms",
+                    textDecorationColor: "orange",
+                  }}
+                >
+                  {element}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
