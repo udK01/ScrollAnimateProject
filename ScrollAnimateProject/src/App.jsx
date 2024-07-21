f8c7d729f23c67ec3dc1d601cf8f386667ededf2
udK01 <dominik.muranyi@gmail.com>
2024-07-21 13:56:59 +0100

    Did a bunch of spline testing.

--- ScrollAnimateProject/src/App.jsx
+++ ScrollAnimateProject/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import Education from "./components/Education";
 import AboutMe from "./components/AboutMe";
